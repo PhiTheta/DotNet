d301b2d3262abf392c460aa7d0b99f33d57d2178
millom <mikael.lundstrom@studentconsulting.com>
2016-02-10 13:53:22 +0100

    tst

---
+++ WebApplication12_Github/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication12_Github.MvcApplication" Language="C#" %>
---
+++ WebApplication12_Github/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication12_Github.Startup))]
+namespace WebApplication12_Github
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
